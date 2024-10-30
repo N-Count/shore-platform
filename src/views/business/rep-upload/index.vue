976b08ea20993961f432dceeee2f264ecd2b84df
willson <13580564431@163.com>
2024-10-30 17:54:32 +0800

    增加接口测试按钮

--- src/views/business/rep-upload/index.vue
+++ src/views/business/rep-upload/index.vue
@@ -1,10 +1,34 @@
 <template>
   <d2-container>
-    111
+    <el-button type="primary" @click="click()">测试</el-button>
+    <div>{{ message }}</div>
+    <div>{{ getDate }}</div>
   </d2-container>
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      message: 'Hello, Vue.js!',
+      getDate: {}
+    }
+  },
+  methods: {
+    click() {
+      this.$message.info('Button clicked!')
+      this.getDate = {
+        name: 'getDate',
+        children: [
+          {
+            name: 'getTime',
+            value: 123
+          }
+        ]
+      }
+    }
+  }
+}
 
 </script>
 
